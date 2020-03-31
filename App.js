90d200e4f7379744501d747a72f3ca87bb43a3b6
Ebad ullah Qureshi <ebad.qureshi922@gmail.com>
2020-03-31 01:07:50 -0400

    remove gitignore files

--- App.js
+++ App.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import { StyleSheet, KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard, View } from 'react-native';
-import Header from './Components/header';
-import Footer from './Components/footer';
+import OrderSummary from './Screens/OrderSummary';
+
 import CardPayment from './Components/cardPayment';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Header title='Order Summary' />
-      <View style={{flex:1}}>
-        <CardPayment />
-      </View>
-      <Footer title='Proceed to Checkout' />
+      <OrderSummary/>      
     </View>
   );
 }
